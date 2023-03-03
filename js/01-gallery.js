efafec5deb2b331d7e3ef602cabba6931b7fb745
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-03-03 14:22:21 +0200

    #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+
+function createGalleryMarkup (galleryItems) {
+    return galleryItems
+    .map(( {preview, original, description} ) => {
+        
+        return  `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+        <img
+        class="gallery__image"
+        src="${preview} "
+        data-source="${original} "
+        alt="${description} "
+      />
+    </a>
+  </div> 
+    `
+    })
+    .join("");
+
+}
+
+galleryContainer.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onClickEsc);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onClickEsc);
+      },
+    }
+  );
+
+  function onClickEsc(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+
+  instance.show();
+}
