835b993e53c3149f7f75968b9a0f8ea5ce6fa0b3
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-03-03 22:35:29 +0200

    #2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -14,12 +14,12 @@ function createGalleryMarkup (galleryItems) {
     .map(( {preview, original, description} ) => {
         
         return  `<div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
+        <a class="gallery__link" href="${original}">
         <img
         class="gallery__image"
-        src="${preview} "
-        data-source="${original} "
-        alt="${description} "
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
       />
     </a>
   </div> 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<a class="gallery__item" href=" ${original}">
+        <img class="gallery__image" src="${preview} " alt="${description} " />
+      </a>
+        `;
+
+    }).join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery .gallery__item", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
 console.log(galleryItems);
